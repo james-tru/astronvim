806a5cadd619fbc30e1659eaa1bd80cf4d95b1ba
james-tru <james@trufintech.io>
2024-08-13 13:39:06 +0100

    First go at setup

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+# if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -9,4 +9,20 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.rust" },
+  -- { import = "astrocommunity.completion.tabnine-nvim"},
+  -- { import = "astrocommunity.bars-and-lines.dropbar-nvim"},
+  { import = "astrocommunity.editing-support.comment-box-nvim"},
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim"},
+  { import = "astrocommunity.editing-support.vim-move"},
+  { import = "astrocommunity.editing-support.wildfire-nvim"},
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.pack.terraform"},
+  { import = "astrocommunity.pack.yaml"},
+  { import = "astrocommunity.utility.noice-nvim"},
+  { import = "astrocommunity.motion.nvim-surround"},
+  { import = "astrocommunity.syntax.vim-easy-align"},
+  { import = "astrocommunity.diagnostics.trouble-nvim"},
+  { import = "astrocommunity.pack.markdown"}
 }
---
+++ lua/plugins/mappings.lua
@@ -0,0 +1,80 @@
+return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = {
+      mappings = {
+        -- first key is the mode
+        n = {
+          -- second key is the lefthand side of the map
+          -- mappings seen under group name "Buffer"
+          ["<Leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+          ["<Leader>bD"] = {
+            function()
+              require("astroui.status").heirline.buffer_picker(function(bufnr)
+                require("astrocore.buffer").close(bufnr)
+              end)
+            end,
+            desc = "Pick to close",
+          },
+          -- tables with the `name` key will be registered with which-key if it's installed
+          -- this is useful for naming menus
+          ["<Leader>b"] = { name = "Buffers" },
+          -- quick save
+          -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+          ["<C-u>"] = {"<C-u>zz"},
+          ["<C-d>"] = {"<C-d>zz"},
+          ["n"] = {"nzz"},
+          ["N"] = {"Nzz"},
+          ["<leader>O"] = {name = " Obsidian"},
+          ["<leader>Oo"] = {":ObsidianOpen<SPACE>"},
+          ["<leader>On"] = {":ObsidianNew<SPACE>"},
+          ["<leader>Os"] = {":ObsidianQuickSwitch<cr>"},
+          ["<leader>Ol"] = {":ObsidianFollowLink<SPACE>"},
+          ["<leader>Ob"] = {":ObsidianBacklinks<cr>"},
+          ["<leader>Og"] = {":ObsidianTags<SPACE>"},
+          ["<leader>Ot"] = {":ObsidianToday<SPACE>"},
+          ["<leader>Om"] = {":ObsidianTomorrow<cr>"},
+          ["<leader>Oy"] = {":ObsidianYesterday<SPACE>"},
+          ["<leader>OT"] = {":ObsidianTemplate<SPACE>"},
+          ["<leader>OS"] = {":ObsidianSearch<SPACE>"},
+          ["<leader>OL"] = {":ObsidianLink<SPACE>"},
+          ["<leader>ON"] = {":ObsidianLinkNew<SPACE>"},
+          ["<leader>Ow"] = {":ObsidianWorkspace<SPACE>"},
+          ["<leader>Op"] = {":ObsidianPasteImg<SPACE>"},
+          ["<leader>Or"] = {":ObsidianRename<SPACE>"},
+        },
+        t = {
+          -- setting a mapping to false will disable it
+          -- ["<esc>"] = false,
+          -- ["<esc>"] = {"<C-\\><C-N>"},
+        },
+      },
+    },
+  },
+  {
+    "AstroNvim/astrolsp",
+    ---@type AstroLSPOpts
+    opts = {
+      mappings = {
+        n = {
+          -- this mapping will only be set in buffers with an LSP attached
+          K = {
+            function()
+              vim.lsp.buf.hover()
+            end,
+            desc = "Hover symbol details",
+          },
+          -- condition for only server with declaration capabilities
+          gD = {
+            function()
+              vim.lsp.buf.declaration()
+            end,
+            desc = "Declaration of current symbol",
+            cond = "textDocument/declaration",
+          },
+        },
+      },
+    },
+  },
+}
