83d321e702302d68de3bc8fe68c5c889ec74045e
james-tru <james@trufintech.io>
2024-08-15 21:58:44 +0100

    Another go at inlay hints

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -59,7 +59,10 @@ return {
     -- customize how language servers are attached
     handlers = {
       -- a function without a key is simply the default handler, functions take two parameters, the server name and the configured options table for that server
-      -- function(server, opts) require("lspconfig")[server].setup(opts) end
+      function(server, opts)
+        require("lspconfig")[server].setup(opts)
+        if vim.lsp.inlay_hint then vim.lsp.inlay_hint.enable(true, { 0 }) end
+      end,
 
       -- the key is the server that is being setup with `lspconfig`
       -- rust_analyzer = false, -- setting a handler to false will disable the set up of that language server
